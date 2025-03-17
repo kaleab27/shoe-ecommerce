7a6abd3ba6dd04a29954092254d162bd411dea25
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-17 22:37:00 +0300

    setup clerk for authentication and user management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Link from "next/link";
@@ -26,33 +27,35 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                    disableTransitionOnChange
+        <ClerkProvider>
+            <html lang="en">
+                <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
                 >
-                    <Header />
-                    {children}
-                    <footer className="border-t py-6 md:py-0">
-                        <div className="container px-4 mx-auto flex flex-col md:flex-row justify-between items-center gap-4 md:h-16">
-                            <p className="text-sm text-muted-foreground">
-                                © 2025 NOBLEMAN. All rights reserved.
-                            </p>
-                            <div className="flex gap-4 text-sm text-muted-foreground">
-                                <Link href="#">Terms</Link>
-                                <Link href="#">Privacy</Link>
-                                <Link href="#">Shipping</Link>
-                                <Link href="#">Returns</Link>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange
+                    >
+                        <Header />
+                        {children}
+                        <footer className="border-t py-6 md:py-0">
+                            <div className="container px-4 mx-auto flex flex-col md:flex-row justify-between items-center gap-4 md:h-16">
+                                <p className="text-sm text-muted-foreground">
+                                    © 2025 NOBLEMAN. All rights reserved.
+                                </p>
+                                <div className="flex gap-4 text-sm text-muted-foreground">
+                                    <Link href="#">Terms</Link>
+                                    <Link href="#">Privacy</Link>
+                                    <Link href="#">Shipping</Link>
+                                    <Link href="#">Returns</Link>
+                                </div>
                             </div>
-                        </div>
-                    </footer>
-                </ThemeProvider>
-            </body>
-        </html>
+                        </footer>
+                    </ThemeProvider>
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
