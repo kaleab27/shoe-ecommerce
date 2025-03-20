692aa3dc556b09d1171dc5516b0c5c64da3316b7
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-20 23:43:59 +0300

    a route to get list of orders

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -24,3 +24,15 @@ export async function POST(req: Request) {
         return new Response("Error creating order", { status: 500 });
     }
 }
+
+export async function GET() {
+    try {
+        const orders = await db.select().from(orderItemsTable);
+        return new Response(JSON.stringify(orders), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new Response("Error Fetching Product Categories", {
+            status: 500,
+        });
+    }
+}
