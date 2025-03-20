c2d110cd4c28a5462de9f87e52e4b99d2a09c717
habtesh-beki <habteshbaki2014@gmail.com>
2025-03-20 16:08:58 -0400

    added: add get and post products

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,34 @@
+import db from "@/db";
+import { productsTable } from "@/db/schema";
+
+export async function GET(req: Request) {
+    try {
+        const products = await db.select().from(productsTable);
+        if (!products) throw new Error("can't find products");
+        return Response.json({
+            status: 200,
+            data: products,
+        });
+    } catch (err: unknown) {
+        return Response.json({
+            status: 400,
+            message: err,
+        });
+    }
+}
+
+export async function POST(req: Request) {
+    try {
+        const reqBody = await req.json();
+        const product = await db.insert(productsTable).values(reqBody);
+        return Response.json({
+            status: "success",
+            data: product,
+        });
+    } catch (err: unknown) {
+        return Response.json({
+            status: 400,
+            message: err,
+        });
+    }
+}
