c444784c8349660424323450ae21f91759639c3a
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-20 23:41:42 +0300

    a route to create orders

---
+++ src/app/api/order/route.ts
@@ -0,0 +1,26 @@
+import db from "@/db/index";
+import { ordersTable, orderItemsTable } from "@/db/schema";
+
+export async function POST(req: Request) {
+    console.log(req);
+    try {
+        const body = await req.json();
+        const [order] = await db
+            .insert(ordersTable)
+            .values(body.order)
+            .returning({ id: ordersTable.id });
+        console.log(order);
+
+        const orderItemsWithOrderId = body.orderItems.map((item: any) => ({
+            ...item,
+            orderId: order.id,
+        }));
+
+        console.log(orderItemsWithOrderId);
+        await db.insert(orderItemsTable).values(orderItemsWithOrderId);
+        return new Response(JSON.stringify(body), { status: 200 });
+    } catch (error) {
+        console.log(error);
+        return new Response("Error creating order", { status: 500 });
+    }
+}
