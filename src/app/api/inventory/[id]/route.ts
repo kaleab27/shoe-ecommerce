dc083324898425d59cf65368714460fe5aadafca
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-25 19:44:19 +0300

    edit parameter for api route

--- src/app/api/inventory/[id]/route.ts
+++ src/app/api/inventory/[id]/route.ts
@@ -2,7 +2,11 @@ import { eq } from "drizzle-orm";
 import db from "@/db";
 import { inventoryTable } from "@/db/schema";
 
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
+export async function GET(
+    req: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    console.log(req);
     const { id } = await params;
     try {
         const orderItems = await db
