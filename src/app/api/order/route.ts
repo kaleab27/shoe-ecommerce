c03b1328641c3f233afa15819f422f3936a178dc
kaleab tadesse <kaleabtadesse49@gmail.com>
2025-03-25 11:10:41 +0300

    group order and orderItem creation into a single transaction

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -5,19 +5,32 @@ export async function POST(req: Request) {
     console.log(req);
     try {
         const body = await req.json();
-        const [order] = await db
-            .insert(ordersTable)
-            .values(body.order)
-            .returning({ id: ordersTable.id });
-        console.log(order);
+        // const [order] = await db
+        //     .insert(ordersTable)
+        //     .values(body.order)
+        //     .returning({ id: ordersTable.id });
+        // console.log(order);
 
-        const orderItemsWithOrderId = body.orderItems.map((item: any) => ({
-            ...item,
-            orderId: order.id,
-        }));
+        // const orderItemsWithOrderId = body.orderItems.map((item: any) => ({
+        //     ...item,
+        //     orderId: order.id,
+        // }));
 
-        console.log(orderItemsWithOrderId);
-        await db.insert(orderItemsTable).values(orderItemsWithOrderId);
+        // console.log(orderItemsWithOrderId);
+        // await db.insert(orderItemsTable).values(orderItemsWithOrderId);
+        await db.transaction(async (tx) => {
+            const [order] = await tx
+                .insert(ordersTable)
+                .values(body.order)
+                .returning({ id: ordersTable.id });
+            console.log(order);
+            const orderItemsWithOrderId = body.orderItems.map((item: any) => ({
+                ...item,
+                orderId: order.id,
+            }));
+            console.log(orderItemsWithOrderId);
+            await tx.insert(orderItemsTable).values(orderItemsWithOrderId);
+        });
         return new Response(JSON.stringify(body), { status: 200 });
     } catch (error) {
         console.log(error);
